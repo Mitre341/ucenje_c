95af0e8dc90392541c7ff9f092c2bc23e94c69f8
Mitre341 <emarkomitrovic@gmail.com>
2026-01-09 18:17:57 -0500

    Kontra ispis sa 2 while petlje

---
+++ ft_rev_print.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+
+char *ft_rev_print(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	while (i >=0)
+	{
+	write(1, &str[i], 1);
+	i--;
+	}
+	return(str);
+}
+
+int main(void)
+{
+	ft_rev_print("Dobro jutro");
+	write(1, "\n", 1);
+	return (0);
+}
